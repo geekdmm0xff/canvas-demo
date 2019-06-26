64228ffcfef604b1d57ccd748ebcee3610468100
duanmingming <geekdmm@163.com>
2019-06-26 14:34:00 +0800

    support mobile

--- main.js
+++ main.js
@@ -6,33 +6,77 @@ var lastPoint = { x: undefined, y: undefined };
 var eraser = document.querySelector("#eraser");
 var enableEraser = false;
 
-canvas.addEventListener("mousedown", e => {
-  painting = true;
-  let x = e.clientX;
-  let y = e.clientY;
-  lastPoint = { x, y };
-});
-canvas.addEventListener("mousemove", e => {
-  if (!painting) return;
-  let x = e.clientX;
-  let y = e.clientY;
+var brush = document.querySelector("#brush");
 
-  if (enableEraser) {
-    context.clearRect(x, y, 5, 5);
-    return;
-  }
+hasTouch() ? listenTouch() : listenMouse();
 
-  let newPoint = { x, y };
-  drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
-  lastPoint = newPoint;
-});
-canvas.addEventListener("mouseup", e => {
-  painting = false;
-});
+// listen mouse
+function listenMouse() {
+  canvas.addEventListener("mousedown", e => {
+    console.log("mouse down");
+    painting = true;
+    let x = e.clientX;
+    let y = e.clientY;
+    lastPoint = { x, y };
+  });
+  canvas.addEventListener("mousemove", e => {
+    console.log("mouse move");
+    if (!painting) return;
+    let x = e.clientX;
+    let y = e.clientY;
 
+    if (enableEraser) {
+      context.clearRect(x, y, 20, 20);
+      return;
+    }
+
+    let newPoint = { x, y };
+    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+    lastPoint = newPoint;
+  });
+  canvas.addEventListener("mouseup", e => {
+    console.log("mouse up");
+    painting = false;
+  });
+}
+
+// listen touch
+function listenTouch() {
+  canvas.addEventListener("touchstart", e => {
+    painting = true;
+    let x = e.touches[0].clientX;
+    let y = e.touches[0].clientY;
+    lastPoint = { x, y };
+  });
+  canvas.addEventListener("touchmove", e => {
+    if (!painting) return;
+    let x = e.touches[0].clientX;
+    let y = e.touches[0].clientY;
+
+    if (enableEraser) {
+      context.clearRect(x, y, 20, 20);
+      return;
+    }
+
+    let newPoint = { x, y };
+    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+    lastPoint = newPoint;
+  });
+  canvas.addEventListener("touchend", e => {
+    painting = false;
+  });
+}
+
+// Actions
 eraser.addEventListener("click", () => {
   enableEraser = true;
 });
+
+brush.addEventListener("click", () => {
+  enableEraser = false;
+});
+
+// Helper
 
 function drawCircle(x, y) {
   context.beginPath();
@@ -53,6 +97,11 @@ function setupRect() {
   canvas.height = document.documentElement.clientHeight;
 }
 
+function hasTouch() {
+  return "ontouchstart" in window;
+}
+
+// resize
 {
   setupRect();
   window.onresize = function() {
